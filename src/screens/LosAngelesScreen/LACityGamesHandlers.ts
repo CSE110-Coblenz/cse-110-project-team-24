c6e30817bf3071e236a9fa1d4bfffeb81ae0bdac
Patrick Deng <patrickdeng@Patricks-MacBook-Air.local>
2025-11-04 14:22:01 -0800

    added LB, union station games

--- src/screens/LosAngelesScreen/LACityGamesHandlers.ts
+++ src/screens/LosAngelesScreen/LACityGamesHandlers.ts
@@ -2,6 +2,8 @@
 import Konva from "konva";
 import { startLAXGame } from "./LAXgame";
 import { startSMGame } from "./SMgame";
+import { startUSGame } from "./UnionStationGame";
+import { startLBGame } from "./LongBeachGame.ts";
 
 
 export function handleCityClick(cityName: string, layer: Konva.Layer) {
@@ -9,6 +11,10 @@ export function handleCityClick(cityName: string, layer: Konva.Layer) {
     startLAXGame(layer);
   } else if (cityName == "Santa Monica"){
     startSMGame(layer);
+  } else if (cityName == "Union Station"){
+    startUSGame(layer);
+  } else if (cityName == "Long Beach"){
+    startLBGame(layer);
   }else {
     alert(`Only LAX game! ${cityName} is still under development!`);
   }
---
+++ src/screens/LosAngelesScreen/LongBeachGame.ts
@@ -0,0 +1,244 @@
+import Konva from "konva";
+
+let score = 0; 
+
+export function startLBGame(layer: Konva.Layer) {
+
+  layer.find(".popup").forEach((n) => n.destroy());
+
+  const popup = new Konva.Group({
+    name: "popup",
+    x: 250,
+    y: 200,
+  });
+
+  const rect = new Konva.Rect({
+    width: 700,
+    height: 480,
+    fill: "white",
+    stroke: "black",
+    cornerRadius: 12,
+    shadowBlur: 10,
+  });
+  rect.listening(false);
+  popup.add(rect);
+
+  const question = new Konva.Text({
+    text: "What is the functionality of Long Beach?",
+    x: 30,
+    y: 30,
+    fontSize: 22,
+    fontFamily: "Arial",
+    fill: "black",
+    fontStyle: "bold",
+  });
+
+  popup.add(question);
+
+  const options = [
+    { text: "A. Port for Passengers", correct: false },
+    { text: "B. Port for Freights", correct: true },
+    { text: "C. Port for Automobiles", correct: false },
+    { text: "D. All of them", correct: false },
+  ];
+
+  options.forEach((opt, i) => {
+    const btnY = 90 + i * 55;
+
+    const btn = new Konva.Rect({
+      x: 60,
+      y: btnY,
+      width: 380,
+      height: 45,
+      fill: "#2980b9",
+      cornerRadius: 8,
+      name: "optionBtn",
+      shadowBlur: 4,
+    });
+
+    const txt = new Konva.Text({
+      x: 80,
+      y: btnY + 10,
+      text: opt.text,
+      fontSize: 18,
+      fill: "white",
+    });
+
+    popup.add(btn);
+    popup.add(txt);
+    btn.on("click", () => {
+      handleAnswer(opt.correct, layer, popup);
+    });
+  });
+
+
+  layer.add(popup);
+  popup.moveToTop();
+  layer.draw();
+}
+
+function handleAnswer(correct: boolean, layer: Konva.Layer, popup: Konva.Group) {
+  popup.destroy();
+      const rect = new Konva.Rect({
+    width: 700,
+    height: 480,
+    fill: "white",
+    stroke: "black",
+    cornerRadius: 12,
+    shadowBlur: 10,
+  });
+    layer.add(rect);
+  const message = new Konva.Text({
+    x: 260,
+    y: 250,
+    text: correct ? "Correct!" : "Incorrect! Try again",
+    fontSize: 22,
+    fontFamily: "Arial",
+    fill: correct ? "green" : "red",
+  });
+  layer.add(message);
+
+
+  if (correct) score++;
+
+
+  const scoreText = new Konva.Text({
+    x: 260,
+    y: 290,
+    text: `current score: ${score}`,
+    fontSize: 20,
+    fill: "black",
+  });
+  layer.add(scoreText);
+  layer.draw();
+
+  setTimeout(() => {
+    message.destroy();
+    scoreText.destroy();
+    rect.destroy();
+    if (correct) {
+      nextQuestion(layer);
+    } else {
+      startLBGame(layer); 
+    }
+    layer.draw();
+  }, 2000);
+}
+
+function nextQuestion(layer: Konva.Layer) {
+  const popup = new Konva.Group({
+    name: "popup",
+    x: 250,
+    y: 200,
+  });
+
+  const rect = new Konva.Rect({
+    width: 700,
+    height: 600,
+    fill: "white",
+    stroke: "black",
+    cornerRadius: 12,
+    shadowBlur: 10,
+  });
+
+  popup.add(rect);
+
+  const question = new Konva.Text({
+    text: "Which culture zone is Long Beach next to?",
+    x: 30,
+    y: 30,
+    fontSize: 22,
+    fontFamily: "Arial",
+    fill: "black",
+    fontStyle: "bold",
+  });
+
+  popup.add(question);
+
+  const options = [
+    { text: "A. African Culture", correct: false },
+    { text: "B. Asian Culture", correct: false },
+    { text: "C. Latino Culture", correct: true },
+    { text: "D. Caucasian Culture", correct: false },
+  ];
+
+  options.forEach((opt, i) => {
+    const btnY = 90 + i * 55;
+
+    const btn = new Konva.Rect({
+      x: 60,
+      y: btnY,
+      width: 380,
+      height: 45,
+      fill: "#27ae60",
+      cornerRadius: 8,
+      name: "optionBtn",
+      shadowBlur: 4,
+    });
+
+    const txt = new Konva.Text({
+      x: 80,
+      y: btnY + 10,
+      text: opt.text,
+      fontSize: 18,
+      fill: "white",
+    });
+
+    btn.on("click", () => {
+      const correct = opt.correct;
+      popup.destroy();
+          const rect = new Konva.Rect({
+    width: 700,
+    height: 600,
+    fill: "white",
+    stroke: "black",
+    cornerRadius: 12,
+    shadowBlur: 10,
+  }); 
+    layer.add(rect);
+      const msg = new Konva.Text({
+        x: 260,
+        y: 250,
+        text: correct ? "Correct! You finished all questions here!" : "Wrong! Try again",
+        fontSize: 22,
+        fill: correct ? "green" : "red",
+      });
+
+      layer.add(msg);
+      if (correct) score++;
+
+      const scoreText = new Konva.Text({
+        x: 260,
+        y: 290,
+        text: `total score: ${score}/2`,
+        fontSize: 20,
+        fill: "black",
+      });
+      layer.add(rect);
+      layer.add(scoreText);
+      layer.draw();
+          const laxDot = layer.findOne("#LAX") as Konva.Circle;
+        console.log("🔍 laxDot found?", !!laxDot);
+        if (laxDot) {
+            laxDot.fill("green");
+            layer.draw();
+            console.log("✅ new fill color:", laxDot.fill());
+        }
+
+      setTimeout(() => {
+        msg.destroy();
+        scoreText.destroy();
+        rect.destroy();
+        layer.draw();
+      }, 3000);
+    });
+
+    popup.add(btn);
+    popup.add(txt);
+  });
+
+
+  layer.add(popup);
+  layer.draw();
+
+}
