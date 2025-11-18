28c0aae0426703d0af58531df13be2d479d6c3f2
Stickle <64675913+The-True-Stickle@users.noreply.github.com>
2025-11-18 02:14:12 -0800

    Added in the rest of the cities

--- src/screens/CityInfoScreen/CityInfoScreenModel.ts
+++ src/screens/CityInfoScreen/CityInfoScreenModel.ts
@@ -26,12 +26,49 @@ export class CityInfoModel {
         {
             cityIDtype: "boston",
             cityName: "Boston",
-            description: "A historic city with rich colonial heritage and modern charm.",
-            image1Url: "images/boston1.jpg",
-            image2Url: "images/boston2.jpg",
+            description: "As the spiritual capital of the New England states, as the progenitor of the American Revolution and the nation, and as the earliest centre of American culture, Boston has influenced the country for some three centuries. Though Boston, like New England in general, has played a lessening role in national life since the early 20th century, it has remained the focal point of what may be the most diversified and dynamic combination of educational, cultural, medical, and scientific activities in the United States.",
+            image1Url: "../../../public/USmap.png",
+            image2Url: "../../../public/USmap.png",
             minigameTitle: "Boston History Trivia",
             minigameInfo: "How well do you know Boston's history?",
-        }
+        },
+        {
+            cityIDtype: "dc",
+            cityName: "Washington DC",
+            description: "Washington is an extraordinary city, one with multiple personalities: a working federal city, an international metropolis, a picturesque tourist destination, an unmatched treasury of the country’s history and artifacts, and a cosmopolitan center that retains a neighborly small-town ambience. The role Washington plays as the capital of the United States often overshadows its lively local history and its complex political, economic, and social issues. About half the land in Washington is owned by the U.S. government, which pays no taxes on it. Several hundred thousand people in the D.C. metropolitan area work for the federal government.",
+            image1Url: "../../../public/USmap.png",
+            image2Url: "../../../public/USmap.png",
+            minigameTitle: "Boston History Trivia",
+            minigameInfo: "How well do you know Boston's history?",
+        },
+        {
+            cityIDtype: "losangeles",
+            cityName: "Los Angeles",
+            description: "Los Angeles is California's most populous city and the second-most populous in the U.S., known for its role as a global hub for entertainment, culture, and commerce. Situated between the Pacific Ocean and mountains, it is a sprawling city with a diverse population, a car-dependent culture, and distinct, decentralized neighborhoods like Hollywood, Beverly Hills, and Downtown.",
+            image1Url: "../../../public/USmap.png",
+            image2Url: "../../../public/USmap.png",
+            minigameTitle: "Boston History Trivia",
+            minigameInfo: "How well do you know Boston's history?",
+        },
+        {
+            cityIDtype: "sandiego",
+            cityName: "San Diego",
+            description: "San Diego is a major California city known for its mild climate, 70 miles of coastline, and numerous beaches. It's a cultural hub with attractions like the famous San Diego Zoo and Balboa Park, a significant naval presence, and a growing economy in tech and biotech. The city blends urban sophistication with a laid-back, coastal atmosphere, offering both diverse neighborhoods and extensive outdoor recreation.",
+            image1Url: "../../../public/USmap.png",
+            image2Url: "../../../public/USmap.png",
+            minigameTitle: "Boston History Trivia",
+            minigameInfo: "How well do you know Boston's history?",
+        },
+        {
+            cityIDtype: "chicago",
+            cityName: "Chicago",
+            description: "Chicago is the third-largest city in the U.S. and a major hub for culture, commerce, and transportation, situated on the southwestern shore of Lake Michigan. Known for its iconic architecture, vibrant arts and music scene, and diverse food, the city is organized into 77 distinct neighborhoods. Historically, it became the nation's railroad hub and a key freshwater trading center, playing a crucial role in westward expansion and commerce.",
+            image1Url: "../../../public/USmap.png",
+            image2Url: "../../../public/USmap.png",
+            minigameTitle: "Boston History Trivia",
+            minigameInfo: "How well do you know Boston's history?",
+        },
+
     ];
 
     getCityInfo(cityIDtype: string): CityInformationEntry | undefined {
